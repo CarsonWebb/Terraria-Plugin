c78d9d89a9e1567bc40a6f0eb1438094ed5cc24d
Carson Webb <carsonwebb14@gmail.com>
2025-10-19 13:03:57 -0400

    Added Cloud in a bottle functionality?

--- TerraiaTools/src/main/java/me/carson/terrariaTools/accesories/CloudInBottle.java
+++ TerraiaTools/src/main/java/me/carson/terrariaTools/accesories/CloudInBottle.java
@@ -1,5 +1,6 @@
 package me.carson.terrariaTools.accesories;
 
+import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.ChatColor;
@@ -17,11 +18,13 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class CloudInBottle implements Listener {
 
     private final NamespacedKey key;
     private final NamespacedKey uncraftableKey;
+    private boolean activated=false;
 
     public CloudInBottle(JavaPlugin plugin) {
         this.key = new NamespacedKey(plugin, "cloud");
@@ -50,8 +53,49 @@ public class CloudInBottle implements Listener {
         ItemStack glass = player.getInventory().getItemInMainHand();
         double height=player.getLocation().getY();
         if(glass.getType() == Material.GLASS_BOTTLE&&((height>=180)&&(height<=200))){
-            player.getInventory().remove(glass);
+            glass.setAmount(glass.getAmount() - 1);
             player.getInventory().addItem(createItem());
         }
     }
+
+    @EventHandler
+    public void onShiftRightClick(PlayerInteractEvent event){
+        if (event.getItem() == null) return;
+        if (!(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+
+        Player player = event.getPlayer();
+        if(!player.isSneaking()) return;
+
+        ItemStack item = event.getItem();
+        if (item == null || !item.hasItemMeta()) return;
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer data = meta.getPersistentDataContainer();
+        if (!data.has(key, PersistentDataType.BYTE)) return;
+
+        if(!activated){
+            meta.setEnchantmentGlintOverride(true);
+            item.setItemMeta(meta);
+            //meta.addEnchant(org.bukkit.enchantments.Enchantment.LUCK, 1, true);
+            //meta.addItemFlags(org.bukkit.inventory.ItemFlag.HIDE_ENCHANTS);
+            activated=true;
+        }else{
+            meta.setEnchantmentGlintOverride(false);
+            item.setItemMeta(meta);
+            activated=false;
+        }
+    }
+
+    @EventHandler
+    public void onJump(PlayerJumpEvent event){
+        Player player = event.getPlayer();
+        if(!player.getInventory().contains(createItem())){
+            return;
+        }
+        if(!player.isOnGround()){
+            player.setVelocity(player.getVelocity().add(new Vector(0,1,0)));
+            player.getWorld().playSound(player.getLocation(), "minecraft:entity.breeze.wind_burst", 1f, 1f);
+            player.getWorld().spawnParticle(org.bukkit.Particle.CLOUD, player.getLocation(), 20, 0.2, 0.2, 0.2, 0.05);
+        }
+    }
+
 }
--- TerraiaTools/src/main/java/me/carson/terrariaTools/tools/MomentumCapacitor.java
+++ TerraiaTools/src/main/java/me/carson/terrariaTools/tools/MomentumCapacitor.java
@@ -1,14 +1,9 @@
 package me.carson.terrariaTools.tools;
 
-
-import io.papermc.paper.datacomponent.DataComponentTypes;
-import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.Particle;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -18,9 +13,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 import org.bukkit.*;
 import org.bukkit.event.EventHandler;
